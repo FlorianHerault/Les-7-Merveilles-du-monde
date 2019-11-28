b69c06ae87a8d9e064c076d19458512dbeb3c97b
hugoberthon <43930403+hugoberthon@users.noreply.github.com>
2019-11-28 11:56:45 +0100

    Save du 28/11/2019 Matin

---
+++ WpfApp1/WpfApp1/Consultation_Emprunt.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PPe_C_tranchant_v1
+{
+    /// <summary>
+    /// Logique d'interaction pour Consultation_Emprunt.xaml
+    /// </summary>
+    public partial class Consultation_Emprunt : Window
+    {
+        public Consultation_Emprunt()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MonProfil.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PPe_C_tranchant_v1
+{
+    /// <summary>
+    /// Logique d'interaction pour MonProfil.xaml
+    /// </summary>
+    public partial class MonProfil : Window
+    {
+        public MonProfil()
+        {
+            InitializeComponent();
+        }
+    }
+}
